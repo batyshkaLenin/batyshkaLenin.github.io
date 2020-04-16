096f8dad882f0adefcd7ada44e57044f23a28b7c
batyshkaLenin <auroragamesproject@gmail.com>
2020-04-16 10:11:46 +0700

    Added h-card classes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -13,27 +13,39 @@ const Footer = () => {
       <section className={styles.contacts}>
         <ul className={styles.contactList}>
           <li className={styles.contactItem}>
-            <a href="https://github.com/batyshkaLenin" rel="me">
+            <a
+              className="h-card"
+              href="https://github.com/batyshkaLenin"
+              rel="me"
+            >
               <i className="icon-github-circled" />
             </a>
           </li>
           <li className={styles.contactItem}>
-            <a href="https://vk.com/batyshkalenin" rel="me">
+            <a className="h-card" href="https://vk.com/batyshkalenin" rel="me">
               <i className="icon-vkontakte" />
             </a>
           </li>
           <li className={styles.contactItem}>
-            <a href="https://soundcloud.com/better_not_be_born" rel="me">
+            <a
+              className="h-card"
+              href="https://soundcloud.com/better_not_be_born"
+              rel="me"
+            >
               <i className="icon-soundcloud" />
             </a>
           </li>
           <li className={styles.contactItem}>
-            <a href="https://twitter.com/batyshkaLenin" rel="me">
+            <a
+              className="h-card"
+              href="https://twitter.com/batyshkaLenin"
+              rel="me"
+            >
               <i className="icon-twitter" />
             </a>
           </li>
           <li className={styles.contactItem}>
-            <a href="https://t.me/batyshka_Lenin" rel="me">
+            <a className="h-card" href="https://t.me/batyshka_Lenin" rel="me">
               <i className="icon-telegram" />
             </a>
           </li>
